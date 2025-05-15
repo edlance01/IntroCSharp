e65162c8222fc616631eca6ba1600a0e57a7888b
Ed Lance <test@test.com>
2025-05-15 17:27:23 -0400

    finished Lab2.1

---
+++ Lab2.1/Aviation/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+Console.Write("Enter an aviation part number:");
+ 
+string? partNumber = Console.ReadLine();
+
+if (string.IsNullOrEmpty(partNumber) )
+{
+    Console.WriteLine("Nothing was entered.");
+}
+else
+{
+    Console.WriteLine($"Your entered an aviation part number named {partNumber}");
+}
+
+Console.Write("\nPlease enter an integer for the quantity of the part you requested: ");
+
+string? quantity = Console.ReadLine();
+
+Console.Write("\nPlease enter a double for the price for the part you requested: ");
+
+string? price = Console.ReadLine();
+
+
+if(string.IsNullOrEmpty(quantity) || string.IsNullOrEmpty(price)) 
+{
+    Console.WriteLine("Neither quanity or price can be empty.");
+}
+else if(!int.TryParse(quantity, out int partQty))
+{
+    Console.WriteLine("Quantity must be an integer");
+}
+else if(!double.TryParse(price, out double partPrice))
+{
+    Console.WriteLine("Quantity must be a decimal.");
+}
+else
+{
+    double partValue = partQty * partPrice;
+    Console.WriteLine($"\nThe total inventory value for {partNumber} is {partValue}");
+}
+
+
