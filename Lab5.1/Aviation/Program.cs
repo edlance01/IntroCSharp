30e526c7ec9e14bc7273987b52c851a1e5f23643
Ed Lance <test@test.com>
2025-05-17 11:13:51 -0400

    finished Lab5.1

---
+++ Lab5.1/Aviation/Program.cs
@@ -0,0 +1,108 @@
+﻿
+
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.CommandController();
+    }
+
+    public void CommandController()
+    { 
+        int totalQuantity = 0;
+        decimal totalValue = 0.0m;
+        while (true)
+        {
+            Console.Write("Please enter a command: add, total, or exit: ");
+            string? command = Console.ReadLine();
+            switch (command)
+            {
+                case "add":
+                    var part = AddAviationPart();
+                    if (part.Number != null)
+                    {
+                        decimal partValue = part.Quantity * part.Price;
+                        Console.WriteLine($"The inventory value for {part.Number} is: {partValue}");
+                        totalQuantity += part.Quantity;
+                        totalValue += partValue;
+                    }
+                    break;
+
+                case "total":
+                    Console.WriteLine($"The total inventory quantity is: {totalQuantity}");
+                    Console.WriteLine($"The total inventory value is: {totalValue}");
+                    break;
+
+                case "exit":
+                    return;
+
+                default:
+                    Console.WriteLine("Unknown command, please enter add, total, or exit.");
+                    break;
+            }
+        }
+                   
+    }
+
+    private (string? Number, int Quantity, decimal Price) AddAviationPart()
+    {
+        string? partNumber = AddAviationPartNumber();
+        if (string.IsNullOrEmpty(partNumber))
+        {
+            return (null, 0, 0m);
+        }
+
+        int partQuantity = AddAviationPartQuantity(partNumber);
+        decimal partPrice = AddAviationPartPrice(partNumber);
+        return (Number: partNumber, Quantity: partQuantity, Price: partPrice);
+    }
+
+    private string? AddAviationPartNumber()
+    {
+ 
+        while (true)
+        {
+            Console.WriteLine("Please enter an aviation part number (or nothing to exit): ");
+            string? partNumber = Console.ReadLine();
+            if (!string.IsNullOrEmpty(partNumber))
+            {
+                return partNumber;
+            }
+            Console.WriteLine("Part number not entered.");
+        }
+
+    }
+
+    private int AddAviationPartQuantity(string partNumber)
+    {
+        while (true)
+        {
+            Console.WriteLine($"Please enter a quantity of part {partNumber}: ");
+            string? quantity = Console.ReadLine();
+            if (int.TryParse(quantity, out int partQuantity) && partQuantity > 0)
+            {
+                Console.WriteLine($"quantity is {quantity}");
+                return partQuantity;
+            }
+            Console.WriteLine("Bad quantity, please enter an integer greater than zero.");
+
+        }
+    }
+
+    private decimal AddAviationPartPrice(string partNumber)
+    {
+        while (true)
+        {
+            Console.WriteLine($"Please enter a price for the part {partNumber}: ");
+            string? price = Console.ReadLine();
+            if (decimal.TryParse(price, out decimal partPrice)  && partPrice > 0)
+            {
+                return partPrice;
+            }
+            Console.WriteLine("Bad price, please enter a decimal price greater than 0.");
+        }
+    }
+
+}
