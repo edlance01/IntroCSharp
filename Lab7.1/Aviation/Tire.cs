0ca6c381aa64ab08b2b51f30846a7f002c325de9
Ed Lance <test@test.com>
2025-05-18 11:29:42 -0400

    finished Lab7.1

---
+++ Lab7.1/Aviation/Tire.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aviation
+{
+    internal class Tire : AviationPart
+    {
+        public double Size { get; set; }
+
+
+        public override string GetPartInfo()
+        {
+            return $"{base.GetPartInfo()}, Tire Size: {Size}";
+           
+        }
+    }
+}
